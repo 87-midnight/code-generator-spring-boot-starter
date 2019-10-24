d53082aabc151965d1b8ef043b3b32b65a946d17
AnnieCattice <AnnieCattice@hotmail.com>
2019-10-24 16:24:32 +0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # code-generator-spring-boot-starter
+
+>代码生成器
---
+++ src/main/java/org/midnight/generator/CodeGenerator.java
@@ -0,0 +1,8 @@
+package org.midnight.generator;
+
+/**
+ * @author linchuangang
+ * @create 2019-10-24 9:36
+ **/
+public class CodeGenerator {
+}
