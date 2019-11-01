dc5e12fcca737ef018f9aa4d4838183d54a79635
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-01 16:33:55 +0800

    定义模板

--- src/main/java/org/midnight/generator/CodeGenerator.java
+++ src/main/java/org/midnight/generator/CodeGenerator.java
@@ -1,7 +1,17 @@
 package org.midnight.generator;
 
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
 
 /**
  * @author linchuangang
@@ -18,5 +28,33 @@ public class CodeGenerator {
 
     public void print(){
         log.info("starter test");
+    }
+
+    public static void main(String...args){
+        Velocity.init();
+        Template template = Velocity.getTemplate("\\src\\main\\resources\\controller\\Controller.vm");
+        VelocityContext ctx = new VelocityContext();
+        ctx.put("list",new ArrayList<Integer>(){
+            {
+                add(1);
+                add(2);
+                add(3);
+            }
+        });
+        ctx.put("beans",new ArrayList<BeansTest>(){
+            {
+                add(BeansTest.builder().name("test1").build());
+                add(BeansTest.builder().name("test2").build());
+            }
+        });
+        Writer writer = new StringWriter();
+        template.merge(ctx, writer);
+        System.out.println(writer.toString());
+    }
+
+    @Data
+    @Builder
+    public static class BeansTest{
+        private String name;
     }
 }
